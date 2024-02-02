0d2a70966b95f6a18ccb40673d9d313cdf271d08
PointlessUsername <42281247+PointlessUser@users.noreply.github.com>
2024-02-02 10:33:04 -0700

    Rename to Fysh

---
+++ src/Fysh.cc
@@ -0,0 +1,64 @@
+#include "Fysh.h"
+#include <cassert>
+
+Species Fysh::type() const noexcept { return Species; }
+
+bool Fysh::isOneOf(Species type1, Species type2) const noexcept {
+  return Species == type1 || Species == type2;
+}
+
+std::string_view Fysh::val() const noexcept { return value; }
+
+constexpr const char *debugType(const Species &type) {
+  switch (type) {
+  case Species::FYSH_LITERAL:
+    return "LITERAL";
+  case Species::FYSH_IDENTIFIER:
+    return "IDENT";
+  case Species::HEART_MULTIPLY:
+    return "*";
+  case Species::DIVIDE:
+    return "/";
+  case Species::TADPOLE_LT:
+    return "<";
+  case Species::TADPOLE_GT:
+    return ">";
+  case Species::TADPOLE_LTE:
+    return "<=";
+  case Species::TADPOLE_GTE:
+    return ">=";
+  case Species::EQUAL:
+    return "==";
+  case Species::NOT_EQUAL:
+    return "!=";
+  case Species::INCREMENT:
+    return "++";
+  case Species::DECREMENT:
+    return "--";
+  case Species::FYSH_TANK_OPEN:
+    return "[";
+  case Species::FYSH_TANK_CLOSE:
+    return "]";
+  case Species::FYSH_OPEN:
+    return "{";
+  case Species::FYSH_CLOSE:
+    return "}";
+  case Species::WTF_OPEN:
+    return "WTF(";
+  case Species::WTF_CLOSE:
+    return ")WTF";
+  case Species::COMMENT:
+    return "//";
+  case Species::OPENING_COMMENT:
+    return "/*";
+  case Species::CLOSING_COMMENT:
+    return "*/";
+  }
+
+  assert(false);
+}
+
+std::ostream &operator<<(std::ostream &os, const Fysh &value) {
+  os << "'" << debugType(value.type()) << "'" << value.val();
+  return os;
+}
--- src/Fysh.h
+++ src/Fysh.h
@@ -1,7 +1,7 @@
 #include <ostream>
 #include <string_view>
 
-enum class TokenType {
+enum class Species {
   FYSH_LITERAL,    // binary value
   FYSH_IDENTIFIER, // variable
   HEART_MULTIPLY,  // <3 or ♡
@@ -27,7 +27,7 @@ enum class TokenType {
   // TODO bit manipulation
 };
 
-class Token {
+class Fysh {
 public:
   // PLEASE DELETE THE EXPLINATION COMMENTS WHEN YOU NO LONGER NEED THEM
 
@@ -43,57 +43,57 @@ public:
   // constructors
   /*
   original:
-  Token (TokenType in_type) {
+  Fysh (Species in_type) {
     this->type = in_type;
   }
 
   short version:
-    Token (TokenType in_type): type(in_type) {}
+    Fysh (Species in_type): type(in_type) {}
 
   The last curly brackets are for the body but its empty
   */
 
   //-----------------Constructors---------------------
 
-  // only the TokenType is given: useful for brackets and stuff since there is
+  // only the Species is given: useful for brackets and stuff since there is
   // only one possible value
-  Token(TokenType inType) noexcept : tokenType{inType} {}
+  Fysh(Species inType) noexcept : Species{inType} {}
 
-  // TokenType, pointer to first element in char array and length of char array
+  // Species, pointer to first element in char array and length of char array
   // is given stores the string_view of the element into value
-  Token(TokenType inType, const char *start, std::size_t len) noexcept
-      : tokenType{inType}, value(start, len) {}
+  Fysh(Species inType, const char *start, std::size_t len) noexcept
+      : Species{inType}, value(start, len) {}
 
-  // TokenType, pointer to start of char array, pointer to end of char array.
+  // Species, pointer to start of char array, pointer to end of char array.
   // gets length and stores into string_view
-  Token(TokenType inType, const char *start, const char *end) noexcept
-      : tokenType{inType}, value(start, std::distance(start, end)) {}
+  Fysh(Species inType, const char *start, const char *end) noexcept
+      : Species{inType}, value(start, std::distance(start, end)) {}
 
   // -----------------------METHODS-----------------------
   // This seems hella unoptimized, I'll modify it once we have a testbench
   template <typename... Ts>
-  bool isOneOf(TokenType type1, TokenType type2, Ts... ks) const noexcept {
-    return tokenType == type1 || isOneOf(type2, ks...);
+  bool isOneOf(Species type1, Species type2, Ts... ks) const noexcept {
+    return Species == type1 || isOneOf(type2, ks...);
   }
 
-  TokenType type() const noexcept;
-  bool isOneOf(TokenType type1, TokenType type2) const noexcept;
+  Species type() const noexcept;
+  bool isOneOf(Species type1, Species type2) const noexcept;
   std::string_view val() const noexcept;
 
-  bool operator==(const TokenType &type) const noexcept {
-    return tokenType == type;
+  bool operator==(const Species &type) const noexcept {
+    return Species == type;
   }
 
-  bool operator==(const Token &other) const noexcept {
-    return other.tokenType == tokenType && other.value == value;
+  bool operator==(const Fysh &other) const noexcept {
+    return other.Species == Species && other.value == value;
   }
 
   const char *toString() const { return "fish"; }
 
 private:
   // curly bracker means initialize to default value (example: 0)
-  TokenType tokenType{};
+  Species Species{};
   std::string_view value{};
 };
 
-std::ostream &operator<<(std::ostream &os, const Token &value);
+std::ostream &operator<<(std::ostream &os, const Fysh &value);
