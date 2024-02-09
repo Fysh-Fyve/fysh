569c521af7f24d320ffdb7f0810709665bafffdc
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2024-02-08 18:52:44 -0700

    Add my name too :)

--- src/Fysh.cc
+++ src/Fysh.cc
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2024 Yahya Al-Shamali, Kyle Prince
+ * Copyright (C) 2024 Yahya Al-Shamali, Kyle Prince, Charles Ancheta
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
