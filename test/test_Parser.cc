bc33d8083e0e55c2d7aaa2ff40253e53e2eb3d38
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2024-02-15 13:01:57 -0700

    Add test for Expression statements

--- test/test_Parser.cc
+++ test/test_Parser.cc
@@ -2,6 +2,7 @@
 #include "../src/Parser.h"
 
 #include "doctest.h"
+#include <cstdint>
 #include <variant>
 
 using namespace fysh::ast;
@@ -11,9 +12,25 @@ template <typename T> T get_stmt(FyshStmt stmt) {
   return std::get<T>(stmt);
 }
 
+template <typename T> T get_expr(FyshStmt stmt) {
+  auto expr{get_stmt<FyshExpr>(stmt)};
+  REQUIRE(std::holds_alternative<T>(expr));
+  return std::get<T>(expr);
+}
+
 template <typename T> T get_expr(FyshExpr expr) {
   REQUIRE(std::holds_alternative<T>(expr));
   return std::get<T>(expr);
+}
+
+template <typename T> void check_ident(T expr, const char *name) {
+  auto ident{get_expr<FyshIdentifier>(expr)};
+  CHECK(ident.name == name);
+}
+
+template <typename T> void check_num(T expr, std::uint64_t value) {
+  auto literal{get_expr<FyshLiteral>(expr)};
+  CHECK(literal.num == value);
 }
 
 TEST_CASE("Assignment Statement") {
@@ -21,19 +38,26 @@ TEST_CASE("Assignment Statement") {
   std::vector<FyshStmt> program{p.parseProgram()};
   REQUIRE(program.size() == 1);
   auto stmt{get_stmt<FyshAssignmentStmt>(program[0])};
-  auto left{get_expr<FyshIdentifier>(stmt.left)};
-  auto right{get_expr<FyshLiteral>(stmt.right)};
-  CHECK(left.name == "fysh");
-  CHECK(right.num == 1);
+  check_ident(stmt.left, "fysh");
+  check_num(stmt.right, 1);
 }
 
-TEST_CASE("Expression Statement") {
-  fysh::FyshParser p{fysh::FyshLexer{"><(({o> ~"}};
+TEST_CASE("Expression Statements") {
+  fysh::FyshParser p{fysh::FyshLexer{R"(
+><(({o> ~
+><fysh> ~
+><fysh> <3 ><{({o> ~
+)"}};
   std::vector<FyshStmt> program{p.parseProgram()};
-  REQUIRE(program.size() == 1);
-  auto stmt{get_stmt<FyshExpr>(program[0])};
-  auto expr{get_expr<FyshLiteral>(stmt)};
-  CHECK(expr.num == 1);
+  REQUIRE(program.size() == 3);
+
+  check_num(program[0], 1);
+  check_ident(program[1], "fysh");
+
+  auto binaryExpr{get_expr<Box<FyshBinaryExpr>>(program[2])};
+  check_ident(binaryExpr.t->left, "fysh");
+  check_num(binaryExpr.t->right, 5);
+  CHECK(binaryExpr.t->op == FyshBinary::Mul);
 }
 
 TEST_CASE("Increment Statement") {
@@ -41,8 +65,7 @@ TEST_CASE("Increment Statement") {
   std::vector<FyshStmt> program{p.parseProgram()};
   REQUIRE(program.size() == 1);
   auto stmt{get_stmt<FyshIncrementStmt>(program[0])};
-  auto expr{get_expr<FyshIdentifier>(stmt.expr)};
-  CHECK(expr.name == "fysh");
+  check_ident(stmt.expr, "fysh");
 }
 
 TEST_CASE("Decrement Statement") {
@@ -50,6 +73,5 @@ TEST_CASE("Decrement Statement") {
   std::vector<FyshStmt> program{p.parseProgram()};
   REQUIRE(program.size() == 1);
   auto stmt{get_stmt<FyshDecrementStmt>(program[0])};
-  auto expr{get_expr<FyshIdentifier>(stmt.expr)};
-  CHECK(expr.name == "fysh");
+  check_ident(stmt.expr, "fysh");
 }
