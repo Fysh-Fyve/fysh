8730a7b153333f1b6bb19eb76a7db62091748328
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2024-02-20 12:57:37 -0700

    Add documentation and fysh eye

--- fysh.sh
+++ fysh.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Translate numbers to fysh, writing to standard output.
+# Usage:
+#	./fysh.sh [NUMBER]
+# With no NUMBER, read standard input.
 
 [ $# = 0 ] && {
 	line=$(cat)
@@ -6,4 +10,4 @@
 	line=$1
 }
 scales=$(echo "obase=2;$line" | bc | tr '1' '{' | tr "0" "(")
-echo "><$scales>"
+echo "><$scales°>"
