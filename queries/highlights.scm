e9ef7358551af55b8db3e6aef7f4754d37591e62
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2024-03-19 11:48:32 -0600

    Add query highlights

---
+++ queries/highlights.scm
@@ -0,0 +1,35 @@
+; TODO: Be more colourful??
+
+(comment) @comment @spell
+
+(positive_ident) @type
+(negative_ident) @type.definition
+
+(one) @punctuation.special
+(zero) @constant
+
+[
+  "["
+  "]"
+  "><>"
+  "<><"
+  "("
+  ")"
+] @punctuation.bracket
+
+(happy_fysh) @keyword
+(loopy_fysh) @keyword
+(dead_fysh) @keyword
+
+; TODO: Finish all operators
+[
+  "<3"
+  "</3"
+  "<<"
+  ">>"
+  "(+o"
+  "o+)"
+  "o~"
+  "~o"
+] @operator
+
