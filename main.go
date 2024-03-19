df78999b39dd758ac6336e30c2f991ebca0eaf72
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2024-03-19 13:16:17 -0600

    Fysh language server

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"context"
+	_ "embed"
+	"fmt"
 	"log"
+	"net/url"
+	"os"
+	"strings"
 
+	fysh "github.com/Fysh-Fyve/fyshls/bindings"
+	sitter "github.com/smacker/go-tree-sitter"
 	"github.com/tliron/glsp"
 	protocol "github.com/tliron/glsp/protocol_3_16"
 	"github.com/tliron/glsp/server"
 )
+
+//go:embed tree-sitter-fysh/queries/highlights.scm
+var highlights []byte
 
 var version string = "0.0.1"
 
@@ -27,27 +38,50 @@ func NewFyshLs() *FyshLs {
 	}
 	return ls
 }
+
 func (ls *FyshLs) completion(
-	context *glsp.Context,
+	_ *glsp.Context,
 	params *protocol.CompletionParams,
 ) (any, error) {
 	// TODO: complete??
-	log.Println("completion", params.TextDocument.URI)
+	f, err := openFileFromURI(params.TextDocument.URI)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	log.Println(f)
 	return []protocol.CompletionItem{}, nil
 }
 
 func (ls *FyshLs) semanticTokensFull(
-	context *glsp.Context,
+	_ *glsp.Context,
 	params *protocol.SemanticTokensParams,
 ) (*protocol.SemanticTokens, error) {
 	log.Println("semantic", params.TextDocument.URI)
-	return &protocol.SemanticTokens{
-		Data: []protocol.UInteger{},
-	}, nil
+	f, err := openFileFromURI(strings.TrimSpace(params.TextDocument.URI))
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	n, err := sitter.ParseCtx(context.Background(), f, fysh.GetLanguage())
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	data := []protocol.UInteger{}
+	log.Println(n.ChildCount())
+	sitter.NewIterator(n, sitter.BFSMode).ForEach(func(node *sitter.Node) error {
+		switch node.Type() {
+		case "zero":
+		case "one":
+		}
+		return nil
+	})
+	return &protocol.SemanticTokens{Data: data}, nil
 }
 
 func (ls *FyshLs) initialize(
-	context *glsp.Context,
+	_ *glsp.Context,
 	params *protocol.InitializeParams,
 ) (any, error) {
 	capabilities := ls.handler.CreateServerCapabilities()
@@ -68,7 +102,7 @@ func (ls *FyshLs) initialize(
 	}, nil
 }
 
-func (ls *FyshLs) shutdown(context *glsp.Context) error {
+func (ls *FyshLs) shutdown(_ *glsp.Context) error {
 	return nil
 }
 
@@ -81,4 +115,39 @@ func (ls *FyshLs) RunStdio() {
 func main() {
 	fysh := NewFyshLs()
 	fysh.RunStdio()
+}
+
+func openFileFromURI(uri string) ([]byte, error) {
+	// Parse the URI
+	u, err := url.Parse(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	// Check if the scheme is 'file'
+	if u.Scheme != "file" {
+		return nil, fmt.Errorf("unsupported scheme: %s", u.Scheme)
+	}
+
+	// Open the file
+	file, err := os.Open(u.Path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Read file contents
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	fileSize := fileInfo.Size()
+	content := make([]byte, fileSize)
+	_, err = file.Read(content)
+	if err != nil {
+		return nil, err
+	}
+
+	return content, nil
 }
