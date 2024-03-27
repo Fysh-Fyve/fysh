704d7f4a09659f10a610878c139e479ad7939cf3
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2024-03-26 22:49:04 -0600

    Refactor

--- cmd/install/main.go
+++ cmd/install/main.go
@@ -10,70 +10,49 @@ import (
 	"time"
 )
 
-var (
-	PACKAGE         = "github.com/Fysh-Fyve/fyshls"
-	VERSION         string
-	COMMIT_HASH     string
-	BUILD_TIMESTAMP string
-)
+const PACKAGE = "github.com/Fysh-Fyve/fyshls"
+
+func X(k, v string) string { return fmt.Sprintf("-X '%s/version.%s=%s'", PACKAGE, k, v) }
+
+func run(args ...string) string {
+	cmd := exec.Command(args[0], args[1:]...)
+	if output, err := cmd.Output(); err == nil {
+		return strings.TrimSpace(string(output))
+	}
+	return ""
+}
 
 func main() {
-	// Retrieve version, commit hash, and build timestamp
-	getVersionInfo()
+	version := run("git", "describe", "--tags", "--always", "--abbrev=0", "--match=v[0-9]*.[0-9]*.[0-9]*")
+	commitHash := run("git", "rev-parse", "--short", "HEAD")
+	buildTimeStamp := time.Now().Format("2006-01-02T15:04:05")
 
 	// Set linker flags
-	ldflags := fmt.Sprintf("-X '%s/version.version=%s' -X '%s/version.commitHash=%s' -X '%s/version.buildTimestamp=%s' -X '%s/version.LogStderr=true'",
-		PACKAGE, VERSION,
-		PACKAGE, COMMIT_HASH,
-		PACKAGE, BUILD_TIMESTAMP,
-		PACKAGE)
+	ldflags := strings.Join(
+		[]string{
+			X("version", version),
+			X("commitHash", commitHash),
+			X("buildTimestamp", buildTimeStamp),
+			X("LogStderr", "true"),
+		},
+		" ",
+	)
 
 	args := os.Args[1:]
 
+	var cmd *exec.Cmd
 	if len(args) > 0 {
-		// Build binary with provided name
-		if err := buildBinary(args[0], ldflags); err != nil {
-			fmt.Println("Error:", err)
-			os.Exit(1)
-		}
+		cmd = exec.Command("go", "build", "-o", args[0], "-ldflags", ldflags)
 	} else {
-		// Install package with linker flags
-		if err := installPackage(ldflags); err != nil {
-			fmt.Println("Error:", err)
-			os.Exit(1)
-		}
-	}
-}
-
-func getVersionInfo() {
-	// Retrieve version from git describe
-	versionCmd := exec.Command("git", "describe", "--tags", "--always", "--abbrev=0", "--match=v[0-9]*.[0-9]*.[0-9]*")
-	if output, err := versionCmd.Output(); err == nil {
-		VERSION = strings.TrimSpace(string(output)[1:]) // Remove leading 'v'
+		cmd = exec.Command("go", "install", "-ldflags", ldflags)
 	}
 
-	// Retrieve commit hash
-	commitHashCmd := exec.Command("git", "rev-parse", "--short", "HEAD")
-	if output, err := commitHashCmd.Output(); err == nil {
-		COMMIT_HASH = strings.TrimSpace(string(output))
-	}
-
-	// Set build timestamp
-	BUILD_TIMESTAMP = time.Now().Format("2006-01-02T15:04:05")
-}
-
-func buildBinary(outputFile, ldflags string) error {
-	cmd := exec.Command("go", "build", "-o", outputFile, "-ldflags", ldflags)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	return cmd.Run()
-}
-
-func installPackage(ldflags string) error {
-	cmd := exec.Command("go", "install", "-ldflags", ldflags, ".")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
+	if err := cmd.Run(); err != nil {
+		fmt.Println("error: ", err)
+		os.Exit(1)
+	}
 }
 
 // Jippitied from this script
