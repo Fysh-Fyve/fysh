b3601ce65b2dcd490b1344e8d9f470f4ad97c7ac
Yahya Al-Shamali <yaalshama@gmail.com>
2024-03-30 01:51:45 -0600

    remove named rules

--- grammar.js
+++ grammar.js
@@ -16,8 +16,8 @@ const PREC = {
     anchor: 2,
     composite_literal: -1,
   },
-  hearts = ["<3", "♡", "</3", "💔","❤️‍🩹"],
-  multiplicative_operators = [ "<<", ">>", "&"].concat(hearts),
+  hearts = ["<3", "♡", "</3", "💔", "❤️‍🩹"],
+  multiplicative_operators = ["<<", ">>", "&"].concat(hearts),
   additive_operators = ["|", "^"],
   comparative_operators = [
     "==",
@@ -40,38 +40,38 @@ const PREC = {
 module.exports = grammar({
   name: "fysh",
 
-  word: ($) => $._name,
+  word: $ => $._name,
 
-  extras: ($) => [
+  extras: $ => [
     $.comment,
     $.multiline_comment,
     /\s/,
   ],
 
-  externals: ($) => [
+  externals: $ => [
     $.multiline_comment,
   ],
 
-  conflicts: ($) => [
+  conflicts: $ => [
     // [$.loop_statement, $.if_statement, $.scales],
     [$.addition],
     [$.if_statement],
   ],
 
   rules: {
-    source_file: ($) => repeat(choice($._statement, $.subroutine)),
+    source_file: $ => repeat(choice($._statement, $.subroutine)),
 
-    subroutine: ($) =>
+    subroutine: $ =>
       seq(
         $.right_sub,
         field("params", repeat($.positive_ident)),
         field("body", $.block),
       ),
 
-    right_sub: ($) => seq(">(", field("name", $._name), ")"),
-    left_sub: ($) => seq("(", field("name", $._name), ")<"),
+    right_sub: $ => seq(">(", field("name", $._name), ")"),
+    left_sub: $ => seq("(", field("name", $._name), ")<"),
 
-    _statement: ($) =>
+    _statement: $ =>
       choice(
         $._simple_statement,
         $.if_statement,
@@ -79,7 +79,7 @@ module.exports = grammar({
         $.block,
       ),
 
-    _simple_statement: ($) =>
+    _simple_statement: $ =>
       seq(
         choice(
           $.assignment_statement,
@@ -93,26 +93,24 @@ module.exports = grammar({
         "~",
       ),
 
-    _statement_list: ($) => seq($._statement, repeat($._statement)),
+    _statement_list: $ => seq($._statement, repeat($._statement)),
 
     return_statement: $ => seq(choice("<~", "🦑"), field("right", $._expression)),
 
-    break_statement: $ => choice("><\\/>","<\\/><"),
+    break_statement: _ => choice("><\\/>", "<\\/><"),
 
-    block: ($) => seq("><>", optional($._statement_list), "<><"),
+    block: $ => seq("><>", optional($._statement_list), "<><"),
 
-    anchor_statement: ($) =>
-      seq(field("dir", anchor), field("value", $._expression)),
+    anchor_statement: $ => seq(field("dir", anchor), field("value", $._expression)),
 
-    loopy_fysh: (_) => controlFysh("@"),
-    happy_fysh: (_) => controlFysh("^"),
-    dead_fysh: (_) => controlFysh("*"),
-    grilled_fysh: (_) => rightFysh("###"),
+    loopy_fysh: _ => controlFysh("@"),
+    happy_fysh: _ => controlFysh("^"),
+    dead_fysh: _ => controlFysh("*"),
+    grilled_fysh: _ => rightFysh("###"),
 
-    loop_statement: ($) =>
-      seq($.loopy_fysh, field("cond", $.fysh_tank), field("body", $.block)),
+    loop_statement: $ => seq($.loopy_fysh, field("cond", $.fysh_tank), field("body", $.block)),
 
-    if_statement: ($) =>
+    if_statement: $ =>
       seq(
         $.happy_fysh,
         field("cond", $.fysh_tank),
@@ -125,20 +123,18 @@ module.exports = grammar({
         ),
       ),
 
-    expression_statement: ($) => $._expression,
-    inc_statement: ($) =>
-      prec(PREC.primary, seq(">", rightFysh(field("name", $._name)))),
-    dec_statement: ($) =>
-      prec(PREC.primary, seq(leftFysh(field("name", $._name)), "<")),
+    expression_statement: $ => $._expression,
+    inc_statement: $ => prec(PREC.primary, seq(">", rightFysh(field("name", $._name)))),
+    dec_statement: $ => prec(PREC.primary, seq(leftFysh(field("name", $._name)), "<")),
 
-    assignment_statement: ($) =>
+    assignment_statement: $ =>
       seq(
         field("left", $.positive_ident),
         choice("≈", "="),
         field("right", $._expression),
       ),
 
-    _expression: ($) =>
+    _expression: $ =>
       choice(
         $.binary_expression,
         $.fysh_bowl, // brackets
@@ -153,27 +149,27 @@ module.exports = grammar({
         $.call_expression,
       ),
 
-    aquarium: ($) => seq("[", dashSep($._expression), "]"),
-    fysh_tank: ($) => seq("[", $._expression, "]"),
-    fysh_bowl: ($) => seq("(", $._expression, ")"),
+    aquarium: $ => seq("[", dashSep($._expression), "]"),
+    fysh_tank: $ => seq("[", $._expression, "]"),
+    fysh_bowl: $ => seq("(", $._expression, ")"),
 
-    positive_ident: ($) => rightFysh(field("name", $._name)),
-    positive_scales: ($) =>
-      rightFysh($.scales, repeat(token.immediate(choice("o", "°")))),
-    positive_bones: ($) => // TODO: float
-      rightFysh($.bones, repeat(token.immediate(choice("o", "°")))),
-    negative_ident: ($) => leftFysh(field("name", $._name)),
-    negative_scales: ($) =>
-      leftFysh(repeat(token.immediate(choice("o", "°"))), $.scales),
-    negative_bones: ($) => // TODO: float
-      leftFysh(repeat(token.immediate(choice("o", "°"))), $.bones),
-    scales: ($) => seq(choice($.one, $.zero), repeat(choice($.one, $.zero))),
-    bones: ($) => seq(choice($.one, $.zero), repeat(choice($.one, $.zero)), $.spine, repeat(choice($.one, $.zero, $.spine))),
-    one: (_) => token(choice("{", "}")),
-    zero: (_) => token(choice("(", ")")),
-    spine: (_) => token('-'),
+    positive_ident: $ => rightFysh(field("name", $._name)),
+    positive_scales: $ => rightFysh(field("scales", $._scales), repeat(token.immediate(choice("o", "°")))),
+    positive_bones: $ =>
+      // TODO: float
+      rightFysh(field("bones", $._bones), repeat(token.immediate(choice("o", "°")))),
+    negative_ident: $ => leftFysh(field("name", $._name)),
+    negative_scales: $ => leftFysh(repeat(token.immediate(choice("o", "°"))), field("scales", $._scales)),
+    negative_bones: $ =>
+      // TODO: float
+      leftFysh(repeat(token.immediate(choice("o", "°"))), field("bones", $._bones)),
+    _scales: $ => seq(choice($.one, $.zero), repeat(choice($.one, $.zero))),
+    _bones: $ => seq($._scales, $.spine, repeat(choice($.one, $.zero, $.spine))),
+    one: _ => token(choice("{", "}")),
+    zero: _ => token(choice("(", ")")),
+    spine: _ => token("-"),
 
-    binary_expression: ($) => {
+    binary_expression: $ => {
       const table = [
         [PREC.multiplicative, choice(...multiplicative_operators)],
         [PREC.additive, choice(...additive_operators)],
@@ -198,7 +194,7 @@ module.exports = grammar({
       );
     },
 
-    call_expression: ($) =>
+    call_expression: $ =>
       seq(
         "[",
         repeat(prec(PREC.primary, $._expression)),
@@ -207,12 +203,11 @@ module.exports = grammar({
         "]",
       ),
 
-    addition: ($) =>
-      seq(field("left", $._expression), field("right", $._expression)),
+    addition: $ => seq(field("left", $._expression), field("right", $._expression)),
 
-    _name: (_) => /[_\p{XID_Start}][_\p{XID_Continue}]*/,
+    _name: _ => /[_\p{XID_Start}][_\p{XID_Continue}]*/,
 
-    comment: (_) => token(seq("><//>", /[^\n]*/)),
+    comment: _ => token(seq("><//>", /[^\n]*/)),
   },
 });
 
@@ -255,7 +250,7 @@ function controlFysh(eye) {
  * @return {SeqRule}
  */
 function dashSep1(rule) {
-  return seq(rule, repeat(seq('-', rule)));
+  return seq(rule, repeat(seq("-", rule)));
 }
 
 /**
@@ -264,5 +259,5 @@ function dashSep1(rule) {
  * @return {ChoiceRule}
  */
 function dashSep(rule) {
-  return optional(dashSep1(rule))
+  return optional(dashSep1(rule));
 }
