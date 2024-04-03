12ffb950d86cf414e1fa73dbc38c5edd27dde7c3
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2024-04-03 17:26:05 -0600

    Add get_seg

--- sw/gpio.c
+++ sw/gpio.c
@@ -19,3 +19,27 @@ void fysh_gpio_write(const uint32_t pin, const uint32_t value) {
 }
 
 uint32_t counter_read() { return *counter_address; }
+
+uint32_t get_seg(uint32_t val) {
+  switch (val) {
+    // clang-format off
+    case 0x0: return 0b0111111;
+    case 0x1: return 0b0000110;
+    case 0x2: return 0b1011011;
+    case 0x3: return 0b1001111;
+    case 0x4: return 0b1100110;
+    case 0x5: return 0b1101101;
+    case 0x6: return 0b1111101;
+    case 0x7: return 0b0000111;
+    case 0x8: return 0b1111111;
+    case 0x9: return 0b1101111;
+    case 0xA: return 0b1110111;
+    case 0xB: return 0b1111100;
+    case 0xC: return 0b0111001;
+    case 0xD: return 0b1011110;
+    case 0xE: return 0b1111001;
+    case 0xF: return 0b1110001;
+    default:  return 0;
+    // clang-format on
+  }
+}
